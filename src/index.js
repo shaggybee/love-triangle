3797bad10cc53b1a30ab5ac984dd2491205c9cfa
shaggybee <kislov.v@gmail.com>
2019-03-05 00:33:03 +0300

    feat: change function getLoveTrianglesCount

--- src/index.js
+++ src/index.js
@@ -4,4 +4,20 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+  var count_triangl = 0;
+  if (preferences.length < 3) return count_triangl;
+  
+  for (var i = 0; i < preferences.length; i++){
+    var first_spichonees, second_spichonees, third_spichonees;
+    
+    if ((first_spichonees = preferences[i]) - 1 == i) continue;
+
+    if ((second_spichonees = preferences[first_spichonees - 1]) == first_spichonees) continue;
+
+    if ((third_spichonees = preferences[second_spichonees - 1]) == second_spichonees) continue;
+
+    if ( i == (third_spichonees - 1)) count_triangl += 1;
+  }
+
+  return count_triangl / 3;
+}
